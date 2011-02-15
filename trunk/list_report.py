eba641f1e1e245ec00403a77fc2dc6c3d984675e
santiagopaleka <santiagopaleka@57bfd024-c69d-11de-a077-756b9c6e95db>
2011-02-15 01:18:09 +0000

    Abrir y guardar en list_reporto, solo diálogos

--- trunk/list_report.py
+++ trunk/list_report.py
@@ -1,12 +1,17 @@
 import wx
-import sys, random
+import os, sys, random
 import gettext
 _ = gettext.gettext
+wildcard = "Tex Sep por comas (*.cvs)|*.cvs|"     \
+           "Compiled Python (*.pyc)|*.pyc|" \
+           "SPAM files (*.spam)|*.spam|"    \
+           "Egg file (*.egg)|*.egg|"        \
+           "All files (*.*)|*.*"
 
 data=[]
-
+columnas = ["ID","Peso","TimeStamp"]
 class ListaFrame(wx.Frame):
-    def __init__(self,data):
+    def __init__(self,data,columnas=columnas):
         wx.Frame.__init__(self, None, -1,
                           "wx.ListCtrl in wx.LC_REPORT mode",
                           size=(600,400), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
@@ -35,14 +40,14 @@ class ListaFrame(wx.Frame):
 
         self.m_menubar1 = wx.MenuBar( 0 )
         self.m_menu1 = wx.Menu()
-        self.m_menuItem1 = wx.MenuItem( self.m_menu1, wx.ID_ANY, _("Abrir"), wx.EmptyString, wx.ITEM_NORMAL )
-        self.m_menu1.AppendItem( self.m_menuItem1 )
+        self.m_abrir = wx.MenuItem( self.m_menu1, wx.ID_ANY, _("Abrir"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.m_menu1.AppendItem( self.m_abrir )
         
-        self.m_menuItem2 = wx.MenuItem( self.m_menu1, wx.ID_ANY, _("Guardar"), wx.EmptyString, wx.ITEM_NORMAL )
-        self.m_menu1.AppendItem( self.m_menuItem2 )
+        self.m_guardar = wx.MenuItem( self.m_menu1, wx.ID_ANY, _("Guardar"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.m_menu1.AppendItem( self.m_guardar )
         
-        self.m_menuItem3 = wx.MenuItem( self.m_menu1, wx.ID_ANY, _("Cerrar"), wx.EmptyString, wx.ITEM_NORMAL )
-        self.m_menu1.AppendItem( self.m_menuItem3 )
+        self.m_cerrar = wx.MenuItem( self.m_menu1, wx.ID_ANY, _("Cerrar"), wx.EmptyString, wx.ITEM_NORMAL )
+        self.m_menu1.AppendItem( self.m_cerrar )
         
         self.m_menubar1.Append( self.m_menu1, _("Lista") ) 
         
@@ -50,7 +55,8 @@ class ListaFrame(wx.Frame):
         
         
         # Add some columns
-        for col, text in [[0,"ID"],[1,"peso"],[2,"timestamp"]]:
+        
+        for col, text in enumerate(columnas):#[[0,"ID"],[1,"peso"],[2,"timestamp"]]:
             self.lista.InsertColumn(col, text)
 
         # add the rows
@@ -64,11 +70,88 @@ class ListaFrame(wx.Frame):
         self.lista.SetColumnWidth(1, 150)
         self.lista.SetColumnWidth(2, 150)
         #self.lista.SetColumnWidth(3, wx.LIST_AUTOSIZE_USEHEADER)
+    
+        self.Bind( wx.EVT_MENU, self.OnMenuAbrir, id = self.m_abrir.GetId() )
+        self.Bind( wx.EVT_MENU, self.OnMenuGuardar, id = self.m_guardar.GetId() )
+        self.Bind( wx.EVT_MENU, self.OnMenuCerrar, id = self.m_cerrar.GetId() )
+    
+    
+    def OnMenuAbrir(self, evt):
+        #self.log.WriteText("CWD: %s\n" % os.getcwd())
+        """ Crea el dialogo para abrir un archivo. Se fuerza el directorio actual."""
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir=os.getcwd(), 
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.OPEN | wx.MULTIPLE | wx.CHANGE_DIR
+            )
+    
+        # Show the dialog and retrieve the user response. If it is the OK response, 
+        # process the data.
+        if dlg.ShowModal() == wx.ID_OK:
+            # This returns a Python list of files that were selected.
+            paths = dlg.GetPaths()
+    
+            #self.log.WriteText('You selected %d files:' % len(paths))
+    
+            #for path in paths:
+            #   self.log.WriteText('           %s\n' % path)
+    
+        # Compare this with the debug above; did we change working dirs?
+        #self.log.WriteText("CWD: %s\n" % os.getcwd())
+    
+        # Destroy the dialog. Don't do this until you are done with it!
+        # BAD things can happen otherwise!
+        dlg.Destroy()
+    
+    def OnMenuGuardar(self, evt):
+        dlg = wx.FileDialog(
+            self, message="Save file as ...", defaultDir=os.getcwd(), 
+            defaultFile="", wildcard=wildcard, style=wx.SAVE
+            )
+
+        # This sets the default filter that the user will initially see. Otherwise,
+        # the first filter in the list will be used by default.
+        dlg.SetFilterIndex(0)
+
+        # Show the dialog and retrieve the user response. If it is the OK response, 
+        # process the data.
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPath()
+            #self.log.WriteText('You selected "%s"' % path)
+
+            # Normally, at this point you would save your data using the file and path
+            # data that the user provided to you, but since we didn't actually start
+            # with any data to work with, that would be difficult.
+            # 
+            # The code to do so would be similar to this, assuming 'data' contains
+            # the data you want to save:
+            #
+            # fp = file(path, 'w') # Create file anew
+            # fp.write(data)
+            # fp.close()
+            #
+            # You might want to add some error checking :-)
+            #
+
+        # Note that the current working dir didn't change. This is good since
+        # that's the way we set it up.
+        #self.log.WriteText("CWD: %s\n" % os.getcwd())
+
+        # Destroy the dialog. Don't do this until you are done with it!
+        # BAD things can happen otherwise!
+        dlg.Destroy()
+        
+    def OnMenuCerrar(self,evt):
+        self.Destroy()
+        pass
+
 
 
 if __name__ == '__main__':
     app = wx.PySimpleApp()
-    frame = ListaFrame(data)
+    frame = ListaFrame(data,columnas)
     frame.Show()
     app.MainLoop()
     
